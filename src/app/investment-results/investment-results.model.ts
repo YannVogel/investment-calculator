5e8220f6771cef6a1cab827794871f6936bef797
Yann <yann@offroadlabs.com>
2024-11-28 10:35:51 +0100

    end of project

---
+++ src/app/investment-results/investment-results.model.ts
@@ -0,0 +1,10 @@
+interface InvestmentResult {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+  totalInterest: number;
+  totalAmountInvested: number;
+}
+
+export type InvestmentResults = InvestmentResult[];
